91a4ea579080527fff5970cae75434aa40b0792d
areille <areille@tuta.io>
2020-04-12 16:24:31 +0200

    Add prime-factors rust

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,18 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+pub fn factors(mut n: u64) -> Vec<u64> {
+    let mut res = Vec::<u64>::new();
+    while n % 2 == 0 {
+        res.push(2);
+        n /= 2;
+    }
+    let limit = (n as f64).sqrt() as u64;
+    for i in (3..=limit).step_by(2) {
+        while n % i == 0 {
+            res.push(i);
+            n /= i;
+        }
+    }
+    if n > 2 {
+        res.push(n)
+    }
+    res
 }
