84fa358cd563548b4d6997625a17b3be8aad8259
areille <areille@tuta.io>
2020-04-12 17:14:44 +0200

    Add typing armstrong number

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,12 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let digits = num
+    let digits: Vec<u32> = num
         .to_string()
         .chars()
         .map(|d| d.to_digit(10).unwrap())
         .collect::<Vec<_>>();
-    digits
-        .iter()
-        .map(|d| d.pow(digits.len() as u32))
-        .sum::<u32>()
-        == num
+    let sum: u32 = digits.iter().map(|d| d.pow(digits.len() as u32)).sum();
+    sum == num
 }
