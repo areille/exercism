08ddfa8acaa7573bd3c17bc3d61ef7f5923a9995
areille <areille@tuta.io>
2020-04-12 17:11:15 +0200

    Add rust/armstrong-number

---
+++ rust/armstrong-numbers/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let digits = num
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+    digits
+        .iter()
+        .map(|d| d.pow(digits.len() as u32))
+        .sum::<u32>()
+        == num
+}
