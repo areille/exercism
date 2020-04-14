f8693da62213c435b88d829854ac18940f5cdc66
areille <areille@tuta.io>
2020-04-14 17:08:21 +0200

    Add correction

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,40 +1,23 @@
-const STACK_EMPTY: &str = "Stack is empty";
-
-fn is_opening_bracket(ch: char) -> bool {
-    ch == '{' || ch == '[' || ch == '('
-}
-fn is_closing_bracket(ch: char) -> bool {
-    ch == '}' || ch == ']' || ch == ')'
+fn opposite(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        _ => unreachable!(),
+    }
 }
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    if string.is_empty() {
-        return true;
-    }
     let mut stack: Vec<char> = Vec::new();
     for ch in string.chars() {
-        if is_opening_bracket(ch) {
-            stack.push(ch);
-        }
-        // Exit at this point if stack is empty : opening bracket at start
-        if stack.is_empty() && is_closing_bracket(ch) {
-            return false;
-        }
-        if ch == ')' {
-            let last = stack.pop().expect(STACK_EMPTY);
-            if last == '{' || last == '[' {
-                return false;
+        match ch {
+            '{' | '[' | '(' => stack.push(ch),
+            ')' | '}' | ']' => {
+                if stack.pop() != Some(opposite(ch)) {
+                    return false;
+                }
             }
-        } else if ch == '}' {
-            let last = stack.pop().expect(STACK_EMPTY);
-            if last == '(' || last == '[' {
-                return false;
-            }
-        } else if ch == ']' {
-            let last = stack.pop().expect(STACK_EMPTY);
-            if last == '{' || last == '(' {
-                return false;
-            }
+            _ => (),
         }
     }
     stack.is_empty()
