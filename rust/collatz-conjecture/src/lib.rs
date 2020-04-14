8dd8e99a1a0078278f3c4df7a287ffa55fdce85c
areille <areille@tuta.io>
2020-04-14 18:08:55 +0200

    Add collatz-conjecture correction

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,19 +1,8 @@
-fn is_even(number: u64) -> bool {
-    number % 2 == 0
-}
-
-pub fn collatz(mut n: u64) -> Option<u64> {
-    if n == 0 {
-        return None;
+pub fn collatz(n: u64) -> Option<u64> {
+    match n {
+        0 => None,
+        1 => Some(0),
+        n if n % 2 == 0 => collatz(n / 2).map(|x| x + 1),
+        n => collatz(3 * n + 1).map(|x| x + 1),
     }
-    let mut count = 0;
-    while n != 1 {
-        if is_even(n) {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
-        count += 1;
-    }
-    Some(count)
 }
