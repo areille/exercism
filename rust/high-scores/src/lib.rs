aaf04fb876b3e7f8c7080495dc558b1e25e591fb
areille <areille@tuta.io>
2020-04-14 15:23:53 +0200

    Add correction high-scores

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,37 +1,29 @@
 #[derive(Debug)]
-pub struct HighScores {
-    scores_list: Vec<u32>,
+pub struct HighScores<'a> {
+    scores: &'a [u32],
 }
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        HighScores {
-            scores_list: scores.to_vec(),
-        }
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        self.scores_list.as_slice()
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        match self.scores_list.last() {
-            Some(value) => Some(*value),
-            None => None,
-        }
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        self.scores_list.iter().fold(None, |a, b| a.max(Some(*b)))
+        self.scores.iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let mut best = self.scores_list.to_vec();
-        best.sort();
-        best.reverse();
-        if best.len() < 3 {
-            return best.to_vec();
-        }
-        best[0..3].to_vec()
+        let mut best = self.scores.to_vec();
+        best.sort_unstable_by(|a, b| a.cmp(b).reverse());
+        best.truncate(3);
+        best
     }
 }
