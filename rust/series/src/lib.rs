3c94861244a02edcf70154209472be937984e955
areille <areille@tuta.io>
2020-04-14 17:46:36 +0200

    Add rust/series correction

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,15 +1,5 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut res: Vec<String> = Vec::new();
-    if len > digits.len() {
-        return vec![];
-    }
-    for i in 0..=digits.len() - len {
-        let mut string = String::new();
-        for ch in digits.chars().skip(i).take(len) {
-            string.push(ch);
-        }
-        res.push(string);
-    }
-
-    res
+    (0..digits.len() + 1 - len)
+        .map(|i| digits[i..i + len].to_string())
+        .collect()
 }
