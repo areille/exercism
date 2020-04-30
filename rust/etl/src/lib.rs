57b9965107780e41c05381e1737570dcb1c01569
areille <areille@tuta.io>
2020-04-30 18:10:31 +0200

    add correction rust/etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,11 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut res = BTreeMap::<char, i32>::new();
-    for (points, letters) in h {
-        for c in letters {
-            res.entry(c.to_ascii_lowercase()).or_insert(*points);
-        }
-    }
-    res
+    h.iter()
+        .flat_map(|(&key, vec)| vec.iter().map(move |c| (c.to_ascii_lowercase(), key)))
+        .collect()
 }
