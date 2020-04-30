65b5afd5cf303bba7b094a96bf1866f3af8bdf7a
areille <areille@tuta.io>
2020-04-30 16:08:50 +0200

    Correction rust/nucleotide-count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,41 +1,27 @@
 use std::collections::HashMap;
 
-fn is_nucleotide(n: char) -> bool {
-    n == 'A' || n == 'C' || n == 'G' || n == 'T'
+static VALID: [char; 4] = ['A', 'C', 'G', 'T'];
+
+fn validate(c: char) -> Result<char, char> {
+    if VALID.iter().any(|x| *x == c) {
+        Ok(c)
+    } else {
+        Err(c)
+    }
 }
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    match nucleotide {
-        'A' | 'C' | 'G' | 'T' => {
-            let mut count = 0;
-            for c in dna.chars() {
-                if !is_nucleotide(c) {
-                    return Err(c);
-                }
-                if c == nucleotide {
-                    count += 1;
-                }
-            }
-            return Ok(count);
-        }
-        _ => Err(nucleotide),
-    }
+    validate(nucleotide).and_then(|nucleotide| {
+        dna.chars()
+            .map(validate)
+            .collect::<Result<Vec<_>, _>>()
+            .map(|v| v.into_iter().filter(|x| *x == nucleotide).count())
+    })
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut map = std::collections::HashMap::<char, usize>::new();
-
-    map.insert('A', 0);
-    map.insert('T', 0);
-    map.insert('C', 0);
-    map.insert('G', 0);
-
-    for c in dna.chars() {
-        if !is_nucleotide(c) {
-            return Err(c);
-        }
-        let entry = map.entry(c).or_insert(0);
-        *entry += 1;
-    }
-    Ok(map)
+    VALID
+        .iter()
+        .map(|c| count(*c, dna).map(|size| (*c, size)))
+        .collect()
 }
