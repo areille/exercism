c425d9f05708f3b36049b4a0baf71d76bf2ce42c
areille <areille@tuta.io>
2020-11-12 10:31:20 +0100

    Binary search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,11 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+use core::cmp::Ordering;
+
+pub fn find<R: AsRef<[T]>, T: Ord>(array: R, key: T) -> Option<usize> {
+    let array = array.as_ref();
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => find(&array[mid + 1..], key).map(|i| i + mid + 1),
+    }
 }
